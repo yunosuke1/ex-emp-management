a315e29805674b9621562f8916d721343b6b7d4a
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-07 17:12:13 +0900

    EmployeeServiceにupdateメソッドを追加

--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -33,4 +33,13 @@ public class EmployeeService {
 	public Employee showDetail(Integer id) {
 		return repository.load(id);
 	}
+	
+	/**
+	 * 従業員情報を更新
+	 * 
+	 * @param id
+	 */
+	public void update(Employee employee) {
+		repository.update(employee);
+	}
 }
