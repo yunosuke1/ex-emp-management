b6bfd06b1a41dc2c6798feda5f56659819b17395
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-07 13:27:03 +0900

    add insert method

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 @Service
 @Transactional
 public class AdministratorService {
 	@Autowired
-	AdministratorRepository administratorRepository;
+	AdministratorRepository repository;
 	
-	
+	/**
+	 * 管理者情報を挿入する
+	 * 
+	 * @param administrator 管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		repository.Insert(administrator);
+	}
 }
