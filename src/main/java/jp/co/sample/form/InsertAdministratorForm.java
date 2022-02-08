089dc7230304cd1bffe18b501f9803b3070f7caa
yunosuke1 <98927291+yunosuke1@users.noreply.github.com>
2022-02-08 14:15:56 +0900

    Revert "Feature/admin validation"

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,8 +1,5 @@
 package jp.co.sample.form;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 /**
  * 管理者情報登録時に使用するフォーム
  * 
@@ -12,20 +9,11 @@ import javax.validation.constraints.NotBlank;
 public class InsertAdministratorForm {
 	
 	/** 名前 */
-	@NotBlank(message="名前を入力してください")
 	private String name;
-	
 	/** メールアドレス */
-	@NotBlank(message="メールアドレスを入力してください")
-	@Email(message="メールアドレスが不正です")
 	private String mailAddress;
-	
-	
 	/** パスワード */
-	@NotBlank(message="パスワードを入力してください")
 	private String password;
-	
-	
 	public String getName() {
 		return name;
 	}
